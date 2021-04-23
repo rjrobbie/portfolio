94f892a770a685c54272c560ded376f26a17adfb
Rob B <robjames@Robs-MacBook-Pro.local>
2021-04-23 20:32:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rjrobbie-portfolio
---
+++ script.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector(".btnHamburger");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+  document.body.classList.toggle("lockscreen");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+    document.body.classList.remove("lockscreen");
+  });
+});
