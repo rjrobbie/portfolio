b3a5485d516a64cb495b0f37318b5ea00ce5ba8c
Rob Barber <47794235+rjrobbie@users.noreply.github.com>
2021-11-28 15:47:30 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-const navToggle = document.querySelector(".btnHamburger");
-const navLinks = document.querySelectorAll(".nav__link");
+const hamburgerBtn = document.querySelector(".hamburgerBtn")
+const overlay = document.querySelector(".overlay")
+const active = document.querySelector(".active")
+const navLinks = document.querySelectorAll(".nav-link")
+const lockscreen = document.querySelector('.lockscreen')
+const btnX = document.querySelector('.btnX')
+const reset = document.querySelector('.reset')
+const backToTop = document.querySelector('.back-to-top')
 
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-  document.body.classList.toggle("lockscreen");
-});
+
+hamburgerBtn.addEventListener("click", () => {
+  overlay.classList.toggle("active")
+  document.body.classList.toggle("lockscreen")
+  document.body.classList.toggle("btnX")
+})
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-    document.body.classList.remove("lockscreen");
-  });
+    overlay.classList.remove("active");
+    document.body.classList.remove("lockscreen")
+    document.body.classList.remove("btnX")
+  })
+})
+
+reset.addEventListener("click", () => {
+  overlay.classList.remove("active")
+  document.body.classList.remove("lockscreen")
+  document.body.classList.remove("btnX")
+})
+
+backToTop.addEventListener("click", () => {
+  document.body.scrollTop = document.documentElement.scrollTop = 0;
 });
 
-const animal = 'cat';
- 
+
+
+
+
+
